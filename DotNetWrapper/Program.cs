d36f4f06dc80c1b88ca02e640d303bfcd941ba27
Lathika Sunder <lsunder@logic02d44xemd6t.logitech.com>
2024-07-30 14:38:05 +0530

    feat - Multiple display screenshot capture wrapped with .net8

--- DotNetWrapper/Program.cs
+++ DotNetWrapper/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("libScreenshotPlugin.dylib", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void CaptureScreenshot(string filename);
+
+    static void Main(string[] args)
+    {
+        string folderPath = "./screenshots/";
+        string filename = folderPath + "screenshot_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+        Console.WriteLine("Capturing screenshot to " + filename);
+        CaptureScreenshot(filename);
+        Console.WriteLine("Screenshot captured.");
+    }
+}
